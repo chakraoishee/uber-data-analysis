3e73b807cefa5ada0c910b9f9930857d3520c082
Oishee Chakrabarti <63088607+chakraoishee@users.noreply.github.com>
2021-12-26 19:35:20 -0600

    cleaned up code and finished analysis

--- data_analysis.R
+++ data_analysis.R
@@ -1,3 +1,4 @@
+#### SETUP ####
 library(dplyr)
 library(ggplot2)
 library(lubridate)
@@ -12,17 +13,12 @@ uber_jan_june_15 <- read.csv("uber-raw-data-janjune-15.csv")
 # clean data and split datetime
 uber_jan_june_15$date <- as.Date(uber_jan_june_15$Pickup_date)
 uber_jan_june_15$month <- format(uber_jan_june_15$date, "%m")
+uber_jan_june_15$day <- weekdays(uber_jan_june_15$date)
+
 # create dataset to produce month vs. total journeys plot
 total_pickups_month <- uber_jan_june_15 %>% group_by(month) %>%
   summarise(count = n())
-# barplot to analyze the distribution of pickups per month in NYC over the first 6 months of 2015
-ggplot(total_pickups_month, aes(x = month, y = count)) + 
-  geom_bar(stat = "identity", fill = "#bcd4e6") + 
-  scale_y_continuous(labels = comma, breaks = seq(0, 3000000, by = 500000)) +
-  labs(y="total journeys", x = "month")
 
-# scatter plot for total number of pickups each day of the week, per month
-uber_jan_june_15$day <- weekdays(uber_jan_june_15$date)
 # create a dataset for each month
 uber_jan_15 <- uber_jan_june_15 %>% filter(month == '01')
 uber_feb_15 <- uber_jan_june_15 %>% filter(month == '02')
@@ -31,6 +27,7 @@ uber_april_15 <- uber_jan_june_15 %>% filter(month == '04')
 uber_may_15 <- uber_jan_june_15 %>% filter(month == '05')
 uber_june_15 <- uber_jan_june_15 %>% filter(month == '06')
 
+# create datasets for total picups per day in january
 total_pickups_jan_day <- uber_jan_15 %>% group_by(day) %>%
   summarise(count = n())
 total_pickups_feb_day <- uber_feb_15 %>% group_by(day) %>%
@@ -43,7 +40,16 @@ total_pickups_may_day <- uber_may_15 %>% group_by(day) %>%
   summarise(count = n())
 total_pickups_june_day <- uber_june_15 %>% group_by(day) %>%
   summarise(count = n())
-# plot
+
+#### PLOTS ####
+# barplot to analyze the distribution of pickups per month in NYC over the first 6 months of 2015
+ggplot(total_pickups_month, aes(x = month, y = count)) +
+  scale_y_continuous(labels = comma, breaks = seq(0, 3000000, by = 500000)) +
+  labs(y="total journeys", x = "month") +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  geom_bar(stat = "identity", fill = "#bcd4e6")
+
+# scatter plot for total number of pickups each day of the week, per month
 ggplot(NULL, aes(x = factor(day, weekdays(min(uber_jan_june_15$date) + 3:9)), y = count)) +
   labs(x = 'day') + 
   scale_y_continuous(labels = comma, breaks = seq(0, 500000, by = 50000)) +
